7f150867b3bea560532c443d2db90c2c5b14411f
Dthoma <dthomas@sapient.com>
2015-11-05 11:43:45 -0500

    removing example bird from data load

--- sql/load-data.sql
+++ sql/load-data.sql
@@ -30,17 +30,3 @@ VALUES (1, 'CARD', 'Cardinals', 'Robust, seed-eating birds with strong bills.');
 INSERT INTO families
 (id, uid, name, description)
 VALUES (2, 'NWSPROW', 'New World Sparrows', 'Small, plump, brown-grey birds with short tails and stubby, powerful beaks.');
-
--- -----------------------------------------------------------------------------
--- birds
--- -----------------------------------------------------------------------------
-INSERT INTO birds
-(id, name_common, name_scientific, family_id)
-VALUES (1, 'Northern Cardinal', 'Cardinalis cardinalis', 1);
-
-INSERT INTO birds
-(id, name_common, name_scientific, family_id)
-VALUES (2, 'Field Sparrow', 'Spizella pusilla', 2);
-
-
-
